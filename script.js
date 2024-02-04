c7f0d4d230ab69f19b950d940caf1ad74ab74194
Rajeev Bhardwaj <officialrajeevbhardwaj@gmail.com>
2024-02-04 22:59:56 +0530

    add table Converter

--- script.js
+++ script.js
@@ -14,3 +14,31 @@ function addPeriods() {
   // Display the updated text
   outputDiv.value = updatedText;
 }
+
+async function convertKaTeXToMarkdown() {
+  const katexTable = document.getElementById("inputText").value;
+  const outputDiv = document.getElementById("output");
+  function generateLineWithBars(bars) {
+    let line = "|";
+    for (let index = 0; index <= bars; index++) {
+      line = line + "---|";
+    }
+    return line;
+  }
+
+  const tableContent = await katexTable
+    .replace(/\\begin{tabular}{.*}/, "")
+    .replace(/\\end{tabular}/, "")
+    .replace(/\\hline/g, "")
+    .replace(/&/g, "|");
+
+  const tableRows = await tableContent.split("\\\\");
+  const filterdRows = await tableRows.filter((item) => item.includes("|"));
+  const tableMaker = generateLineWithBars(filterdRows[0].match(/\|/g).length);
+  const finalRows = await filterdRows.map((item) => item.replace(/\n/g, ""));
+  const tableFinalRows = finalRows.splice(1, 0, tableMaker);
+  const finalMdTable = await finalRows.join("\n");
+  console.log(tableFinalRows);
+  outputDiv.value = finalMdTable;
+  // return finalMdTable;
+}
